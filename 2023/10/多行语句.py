6fed5a4d1d849d7536a515cbb12b60b0fc73ef46
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2023-10-29 23:25:16 +0900

    Add files via upload

---
+++ 2023/10/多行语句.py
@@ -0,0 +1,25 @@
+a=1
+b=2
+c=3
+ddd=a + \
+    b + \
+    c
+print(ddd)
+
+ccc =[1,2,3
+      ,4,5,
+      6,7,8]
+
+print(ccc)
+
+fff =(1,2,3
+      ,4,5)
+
+print(fff)
+
+xxx={"一二","三四",
+     "五六"}
+
+
+
+print(xxx)
