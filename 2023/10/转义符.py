cbfa1ae0312e0ac86561e0f446a6adcdc9730bd1
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2023-10-29 21:27:43 +0900

    Add files via upload

---
+++ 2023/10/转义符.py
@@ -0,0 +1,16 @@
+# \n 换行
+# \t 制表符
+# \r 覆盖
+# \b 删除
+# \: 两个反斜线表示一个\
+# 原字符：使得转义字符不起作用，写在前面，用r或R
+# 
+# 
+
+print("我的\"名字\"是汤")
+print("我是\nqiaostw汤")
+print("制表符\t制表符")
+print("我爱\r你")
+print("hello\bworld")
+print("\\")
+print("\\b")
