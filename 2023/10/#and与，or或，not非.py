5a30126e78ebd3505a398c602823eb1f15fb0846
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2023-11-01 00:58:07 +0900

    Add files via upload

---
+++ 2023/10/#and与，or或，not非.py
@@ -0,0 +1,13 @@
+#and与，or或，not非
+print(True and False)
+print(True and True)
+print(False and False)
+print(True or False)
+print(True or True)
+print(False or False)
+print(not True)
+print (not False)
+
+a=not True
+b=2>1 and 3>1
+print(b)
