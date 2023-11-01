5690b395da3dacb03369714fd347f13b771f1f2d
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2023-11-02 00:23:39 +0900

    Add files via upload

---
+++ 2023/11/#python空值与内置函数返回值.py
@@ -0,0 +1,7 @@
+#python空值
+#    """
+a =None
+print(None)
+
+r=print("1234")
+print(r) #没有任何返回值输出None
---
+++ 2023/11/#字符串切片.py
@@ -0,0 +1,8 @@
+#字符串切片
+name ="一二三四五六七八九"
+print(name[3:5])
+print(name[:8])
+print(name[3:])
+print(name[:])
+print(name[-3:-1])
+print(name[-7:])
