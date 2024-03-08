866dc86f39888212a497f843e2a94c707ebcb070
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2024-03-08 21:25:14 +0800

    Add files via upload

---
+++ 2024/3/#关于元组列表字符串共同操作.py
@@ -0,0 +1,16 @@
+#关于元组列表字符串共同操作
+a=(1,2,3,4)
+print(len(a))
+print(max(a))
+print(min(a))
+
+b=[10,20,30,40]
+print(len(b))
+print(max(b))
+print(min(b))
+
+
+c="12345我ABC"
+print(len(c))
+print(max(c)) #字符转换ASCII码比较大小
+print(min(c))
