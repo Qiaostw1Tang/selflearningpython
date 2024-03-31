9ad651cbcd4ad0f9bf744dcad6561c11d4e12320
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2024-03-31 10:51:37 +0800

    Add files via upload

---
+++ 2024/3/#字典的操作方法update函数.py
@@ -0,0 +1,9 @@
+#字典的操作方法update函数
+c={"名字":"姓名"}
+b={   "名字":18
+   }
+a={   "名":19
+   }
+c.update(b)
+c.update(a)
+print(c)
---
+++ 2024/3/#成员检测in,in.py
@@ -0,0 +1,7 @@
+#成员检测in,in
+print("4"in"1234")
+print(1 in(1,2,3,4))
+print(1 in[1,2,3])
+print("一二"in{"名字":"一二"})
+print(3 not in (1,2,9))
+print(4 not in [1,2,4,6])
